6670f436b96392fc2dd38d942ce341bdc7c69e13
zakiaRezay <100529586+zakiaRezay@users.noreply.github.com>
2022-03-03 21:24:21 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/registrar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.registrar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/submition/confirmation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class confirmation extends StatelessWidget {
+ final value;
+  confirmation({this.value});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(value,)
+    );
+  }
+}
+
+
